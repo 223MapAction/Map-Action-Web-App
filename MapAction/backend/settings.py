1a95156971879963aafb7c1e0bc443323c700b6c
yugo19 <doucouremohamed8@gmail.com>
2023-12-26 12:54:16 +0000

    config the allowed host

--- MapAction/backend/settings.py
+++ MapAction/backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"),'localhost', '127.0.0.1']
 
 
 # Application definition

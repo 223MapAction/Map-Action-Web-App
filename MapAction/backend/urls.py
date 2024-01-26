a84499b406475e289651f79d5720911ae4d86617
Map Action <lt75425@gmail.com>
2024-01-26 11:13:05 +0000

    upload

--- MapAction/backend/urls.py
+++ MapAction/backend/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
     path('MapApi/', include('Mapapi.urls')),
     path('MapApp/', include('MapApp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

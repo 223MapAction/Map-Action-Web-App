23d1cb900c3ebae099f31cbaaf78a7aa39cb8840
A7640S <109904807+A7640S@users.noreply.github.com>
2024-09-18 12:12:18 +0000

    Update apps.py

--- MapAction/Mapapi/apps.py
+++ MapAction/Mapapi/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MapapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Mapapi'
+
+    def ready(self):
+        import Mapapi.signals

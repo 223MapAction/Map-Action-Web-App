0a5606785be1e999b1d03c6d168df66bccbfcadb
A7640S <109904807+A7640S@users.noreply.github.com>
2024-09-20 09:31:43 +0000

    Update Send_mails.py

--- MapAction/Mapapi/Send_mails.py
+++ MapAction/Mapapi/Send_mails.py
@@ -5,8 +5,12 @@ from django.utils.html import strip_tags
 
 @shared_task
 def send_email(subject, template_name, context, to_email):
-    html_content = render_to_string(template_name, context)
-    text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, 'contact@map-action.com', [to_email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
+    try:
+        html_content = render_to_string(template_name, context)
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives(subject, text_content, 'contact@map-action.com', [to_email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+    except Exception as e:
+        logger.error(f"Erreur lors de l'envoi de l'email: {str(e)}")
+        raise e

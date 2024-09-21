e55bdf9c67b10a178d7074f43a729f4d626bb73c
A7640S <109904807+A7640S@users.noreply.github.com>
2024-09-21 19:31:32 +0000

    Update settings.py

--- MapAction/backend/settings.py
+++ MapAction/backend/settings.py
@@ -346,7 +346,7 @@ LOGGING = {
 
 AUTH_USER_MODEL = 'Mapapi.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.siteground.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'contact@map-action.com'

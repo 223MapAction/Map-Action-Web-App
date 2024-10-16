0c60073750bd4f3b2e4ad982c013a7cbd0edff92
A7640S <109904807+A7640S@users.noreply.github.com>
2024-10-16 13:22:45 +0000

    Update signals.py

--- MapAction/Mapapi/signals.py
+++ MapAction/Mapapi/signals.py
@@ -11,18 +11,21 @@ def notify_organisation_on_collaboration(sender, instance, created, **kwargs):
     if created:
         incident = instance.incident
         user = incident.taken_by
+        requesting_user = instance.user  
+        requesting_organisation = requesting_user.organisation  
         
-        # Vérification que l'utilisateur et son email existent
         if user and user.email:
             try:
-                # Envoi de l'email à l'organisation
                 context = {
                     'incident_id': incident.id,
                     'incident_title': incident.title,  
                     'incident_zone': incident.zone,  
                     'incident_creation_date': incident.created_at,  
                     'organisation': user.organisation,
+                    'requesting_organisation': requesting_organisation 
                 }
+                
+                # Envoi de l'email à l'organisation
                 send_email.delay(
                     subject='Nouvelle demande de collaboration',
                     template_name='emails/collaboration_request.html',
@@ -34,11 +37,13 @@ def notify_organisation_on_collaboration(sender, instance, created, **kwargs):
                 # Création de la notification pour l'organisation
                 Notification.objects.create(
                     user=user,
-                    message=f"Vous avez une nouvelle collaboration pour l'incident {incident.id}",
+                    message=f"L'organisation {requesting_organisation} souhaite collaborer sur l'incident {incident.title} (Zone: {incident.zone}, Date: {incident.created_at.strftime('%d-%m-%Y')})",
                     colaboration=instance
                 )
                 logger.info(f"Notification créée pour l'utilisateur {user.email}.")
                 
             except Exception as e:
                 logger.error(f"Erreur lors de l'envoi de l'email: {str(e)}")
-
+        else:
+            logger.error(f"Email non valide ou manquant pour l'utilisateur {user}. Collaboration annulée.")
+            instance.delete()  
